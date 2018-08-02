07ab2db16554fa45694be10d7d19caa987845681
frumentum <frumentum@posteo.de>
2018-08-02 11:26:20 +0200

    function analyseShoppingCart is written, documentation inclusive -> all tests are green

---
+++ R/analyseShoppingCart.R
@@ -0,0 +1,61 @@
+#' @title analyse the shopping cart
+#' @description This function analyses the content of a online shopping cart. It
+#' looks for the following information: How many items are sold in total, how
+#' many different items do we have, what's the maximum number of one item and
+#' at least how often do we have this 'maximum number of one item' within the
+#' shopping cart.
+#' @param shoppingCart shopping cart as an data.frame with one row for one item
+#' @param ... Variable(s) which shall be used for grouping. Can be given as a
+#' name.
+#' @return It returns a list with four numeric and named values.
+#' @examples
+#' books <- dplyr::tibble(
+#' itemID = 1:5,
+#' name = c(
+#' "Stein der Weisen",
+#' "Kammer des Schreckens",
+#' "Gefangene von Askaban",
+#' "Feuerkelch",
+#' "Orden des Phönix"
+#' )
+#' )
+
+#' set.seed(1)
+#' shoppingCart <- dplyr::sample_n(books, 8, replace = TRUE)
+#' shoppingCart <- dplyr::arrange(shoppingCart, itemID)
+#' analyseShoppingCart(shoppingCart, itemID, name)
+#' @importFrom magrittr '%>%'
+#' @importFrom rlang ':='
+#' @export
+
+analyseShoppingCart <- function(shoppingCart, ...) {
+
+  # use defined variable ... so our function can accept any number of arguments
+  group_by <- dplyr::quos(...)
+
+  # count items of the shopping cart
+  # note: Three "!!!" are necessary to accept any number of grouping variables
+  # as names
+  groupedShoppingCart <- dplyr::count(shoppingCart, !!!group_by)
+
+  # The new column 'n' contains the number of one item, so if we summarize it
+  # we'll get the total number of items
+  itemsInTotal <- sum(groupedShoppingCart$n)
+  # number of rows represents the number of different items we have
+  numberOfDifferentItems <- nrow(groupedShoppingCart)
+  # And how often was the item bought which was bought the most?
+  maxNumberPerItem <- max(groupedShoppingCart$n)
+  # probably there are more than one item with the same 'maximum number per
+  # item'?
+  numberOfMaxima <- groupedShoppingCart %>%
+    dplyr::filter(n == maxNumberPerItem) %>%
+    nrow()
+
+  return(list(
+    "itemsInTotal" = itemsInTotal,
+    "numberOfDifferentItems" = numberOfDifferentItems,
+    "maxNumberPerItem" = maxNumberPerItem,
+    "numberOfMaxima" = numberOfMaxima
+  ))
+
+}
--- tests/testthat/test_that_analyseShoppingCart.R
+++ tests/testthat/test_that_analyseShoppingCart.R
@@ -3,7 +3,7 @@ test_that(
   "analyseShoppingCart returns a list with four information, combination
   3,1,1,3", {
     # These books are available in the shop
-    books <- tibble(
+    books <- dplyr::tibble(
       itemID = 1:5,
       name = c(
         "Stein der Weisen",
@@ -26,19 +26,19 @@ test_that(
     )
 
     # analyse this shopping cart
-    tmpResult <- analyseShoppingCart(shoppingCart)
+    tmpResult <- analyseShoppingCart(shoppingCart, itemID, name)
     # shall be class list...
     expect_is(tmpResult, "list")
-    # shall contain four numeric informtion...
+    # shall contain four integer informtion...
     expect_length(tmpResult, 4)
-    expect_is(unlist(tmpResult, "numeric"))
+    expect_is(unlist(tmpResult), "integer")
     # these information are named as characters...
-    namesOfInformation <- name(unlist(tmpResult))
+    namesOfInformation <- names(unlist(tmpResult))
     expect_is(namesOfInformation, "character")
     # ... and their names should be
-    expect_equal(namesOfInformation[1], "booksInTotal")
-    expect_equal(namesOfInformation[2], "numberOfDifferentBooks")
-    expect_equal(namesOfInformation[3], "maxNumberPerBook")
+    expect_equal(namesOfInformation[1], "itemsInTotal")
+    expect_equal(namesOfInformation[2], "numberOfDifferentItems")
+    expect_equal(namesOfInformation[3], "maxNumberPerItem")
     expect_equal(namesOfInformation[4], "numberOfMaxima")
   }
 )
