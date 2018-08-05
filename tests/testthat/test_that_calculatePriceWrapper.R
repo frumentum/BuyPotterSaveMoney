f73f1a1479de881b08123648b924c104bac79b08
frumentum <frumentum@posteo.de>
2018-08-05 13:32:32 +0200

    first tests are written for the wrapper function calculatePrice()

---
+++ tests/testthat/test_that_calculatePriceWrapper.R
@@ -0,0 +1,45 @@
+context("text_that_calculatePrice")
+
+# These books are available in the shop
+books <- dplyr::tibble(
+  itemID = 1:5,
+  name = c(
+    "Stein der Weisen",
+    "Kammer des Schreckens",
+    "Gefangene von Askaban",
+    "Feuerkelch",
+    "Orden des Phönix"
+  )
+)
+
+set.seed(1)
+shoppingCart <- dplyr::sample_n(books, 15, replace = TRUE) %>%
+  dplyr::arrange(itemID)
+
+discountTibble <- dplyr::tibble(
+  set = 1:5,
+  discount = c(0, 5, 10, 20, 25)
+)
+
+
+test_that(
+  "one function returns best price with information about discount",
+  {
+    priceInfo <- calculatePrice(
+      shoppingCart, discountTibble, pricePerItem = 8
+    )
+
+    # including the intermediate steps
+    priceInfoDetailed <- calculatePrice(
+      shoppingCart, discountTibble, pricePerItem = 8, intermediateSteps = TRUE
+    )
+
+    # test basic structure first
+    expect_is(priceInfo, "numeric")
+    expect_is(priceInfoDetailed, "list")
+    expect_equal(priceInfo, priceInfoDetailed$bestDiscount)
+    iS <- priceInfoDetailed$intermediateSteps
+    expect_is(iS, "list")
+
+  }
+)
